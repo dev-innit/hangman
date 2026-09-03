3914a6a70c15168dd262bfb384e013783e04509b
dev-innit <181482971+dev-innit@users.noreply.github.com>
2026-09-03 21:59:26 +0300

    initializing all the conditions of the game

--- main.py
+++ main.py
@@ -35,3 +35,17 @@ def play_loop():
     elif play_game.lower() == "n":
         print("Thanks for playing! We expect you back again!")
         exit()
+
+def hangman():
+    global count
+    global display
+    global word
+    global already_guessed
+    global play_game
+    limit = 5
+    guess = input("This is the Hangman Word: " + display + " Enter your guess: \n")
+    guess = guess.strip()
+    
+    if len(guess.strip()) == 0 or len(guess.strip()) >= 2 or guess <= "9":
+        print("Invalid Input, Try a letter\n")
+        hangman()
